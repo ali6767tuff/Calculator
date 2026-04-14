e9670a9c54c7ffa3bf100d4eebf05e49d1ac29a2
ali6767tuff <hassm404@gmail.com>
2026-04-14 15:18:48 +0200

    Add project files.

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CEButton(object sender, EventArgs e)
+        {
+
+        }
+        private void CButton(object sender, EventArgs e)
+        {
+
+        }
+        private void DeleteButton(object sender, EventArgs e)
+        {
+
+        }
+        private void NumberButtons(object sender, EventArgs e)
+        {
+
+        }
+        private void OperatorButtons(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DecimalButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EqualsButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+  }
